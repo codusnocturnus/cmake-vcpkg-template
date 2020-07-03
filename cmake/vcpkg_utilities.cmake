b77198e1fca16f0289f9615801ec275eb41975ef
Nathaniel Niles <codusnocturnus@protonmail.com>
2020-07-02 17:13:00 -0700

    CMake tries to run multiple vcpkg installs simultaneously (#12)
    * CMake tries to run multiple vcpkg installs simultaneously
    Fixes #11
    - vcpkg now uses FetchContent rather than ExternalPackage
    - vcpkg builds from its CMakeLists.txt directly, rather than calling bootstrap-vcpkg
    - vcpkg installs made serially dependent (parallel installs fail)

--- cmake/vcpkg_utilities.cmake
+++ cmake/vcpkg_utilities.cmake
@@ -1,36 +1,33 @@
-include(ExternalProject)
+include(FetchContent)
 function (get_vcpkg)
-    if("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows")
-        set(VCPKG_BUILD_CMD bootstrap-vcpkg.bat)
+    if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
         set(VCPKG_BINARY "vcpkg.exe")
         set(OSNAME "windows")
-    elseif("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
-        set(VCPKG_BUILD_CMD bootstrap-vcpkg.sh)
+    elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
         set(VCPKG_BINARY "vcpkg")
         set(OSNAME "linux")
     endif()
-    if("${CMAKE_SIZEOF_VOID_P}" EQUAL "4")
+    if(${CMAKE_SIZEOF_VOID_P} EQUAL "4")
         set(PLATFORMNAME "x86")
-    elseif("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
+    elseif(${CMAKE_SIZEOF_VOID_P} EQUAL "8")
         set(PLATFORMNAME "x64")
     endif()
     set(VCPKG_TRIPLET "${PLATFORMNAME}-${OSNAME}" PARENT_SCOPE)
 
     if(${ARGC} EQUAL 0)
-        # create/use a local vcpkg instance
-        ExternalProject_Add(vcpkg
+        FetchContent_Declare(vcpkg
             GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
             GIT_TAG 2020.06
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND "<SOURCE_DIR>/${VCPKG_BUILD_CMD}"
-            INSTALL_COMMAND ""
-            UPDATE_COMMAND ""
-            STEP_TARGETS build
-            BUILD_BYPRODUCTS "<SOURCE_DIR>/${VCPKG_BINARY}"
         )
-        ExternalProject_Get_Property(vcpkg SOURCE_DIR)
-        set(VCPKG_DIR "${SOURCE_DIR}" PARENT_SCOPE)
-        set(VCPKG_CMD "${SOURCE_DIR}/${VCPKG_BINARY}" PARENT_SCOPE)
+        FetchContent_GetProperties(vcpkg)
+        if(NOT vcpkg_POPULATED)
+            FetchContent_Populate(vcpkg)
+            add_subdirectory(${vcpkg_SOURCE_DIR}/toolsrc
+                ${vcpkg_SOURCE_DIR}
+            )
+        endif()
+        set(VCPKG_DIR "${vcpkg_SOURCE_DIR}" PARENT_SCOPE)
+        set(VCPKG_CMD "${vcpkg_SOURCE_DIR}/${VCPKG_BINARY}" PARENT_SCOPE)
         set(VCPKG_DEPENDENCIES "vcpkg" PARENT_SCOPE)
     else()
         # use an external/existing vcpkg instance
@@ -53,14 +50,21 @@ function (vcpkg_install PACKAGE_NAME)
         ALL
         DEPENDS "${VCPKG_DIR}/packages/${PACKAGE_NAME}_${VCPKG_TRIPLET}/BUILD_INFO"
     )
+    # need to serialize vcpkg installs, so we establish a chain of dependencies
+    if(DEFINED VCPKG_PRIOR_INSTALLED)
+        add_dependencies(get${PACKAGE_NAME}
+            get${VCPKG_PRIOR_INSTALLED}
+        )
+    endif()
+    set(VCPKG_PRIOR_INSTALLED ${PACKAGE_NAME} PARENT_SCOPE)
     list(APPEND VCPKG_DEPENDENCIES "get${PACKAGE_NAME}")
     set(VCPKG_DEPENDENCIES ${VCPKG_DEPENDENCIES} PARENT_SCOPE)
 endfunction()
 
 function (vcpkg_packagefile FILENAME)
     file(STRINGS ${FILENAME} filelist)
-    foreach(filename ${filelist})
-        vcpkg_install(${filename})
+    foreach(pkgname ${filelist})
+        vcpkg_install(${pkgname})
     endforeach()
     set(VCPKG_DEPENDENCIES ${VCPKG_DEPENDENCIES} PARENT_SCOPE)
 endfunction()
